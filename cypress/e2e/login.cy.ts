7501d822306ceac29bb86715a45c3a989de918e2
Félix MARQUET <fpmovie4@gmail.com>
2025-10-25 16:23:30 +0200

    chore: upgrade Next.js to version 16 and update related configurations

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,9 +1,9 @@
 describe('Login', () => {
   it('should login successfully', () => {
-    cy.visit('/login')
-    cy.get('input[id="email"]').type('test@example.com')
-    cy.get('input[id="password"]').type('password')
-    cy.get('button[type="submit"]').click()
-    cy.url().should('include', '/dashboard')
-  })
-})
+    cy.visit('/login');
+    cy.get('input[id="email"]').type('test@example.com');
+    cy.get('input[id="password"]').type('password');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/dashboard');
+  });
+});
